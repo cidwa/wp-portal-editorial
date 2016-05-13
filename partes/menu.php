6ace6fc5242291cf149742dac617e082d07dffcc
salomonmac <geforcesalomonti@gmail.com>
2016-05-13 14:49:40 -0500

    update de fallas Cairo

---
+++ partes/menu.php
@@ -0,0 +1,133 @@
+<!--Walter segundo menu-->
+<div class="container">
+    <nav class="navbar navbar-default navbar-principal ">
+        <div class="container-fluid">
+            <ul class="menu_principal">
+
+
+                <ul class="nav navbar-nav">
+                    <li><a href="#">NUESTROS LIBROS</a></li>
+
+                    <li class="btn-group">
+                        <a href="#" class=" dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">ÁREA TEMÁTICA <b class="caret"></b>
+                                    </a>
+
+                        <div class="container dropdown-menu" id="tabla-container">
+                            <table class="table">
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <a href="">> Estudios de Posgrado</a>
+                                        </td>
+                                        <td>
+                                            <a href="">> Investigación Aplicada y Multidiciplinaria</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="">> Ciencias Juridicas</a>
+                                        </td>
+
+                                        <td>
+                                            <a href="">> Ciencias Socioeconómicas</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <a href="">> Diseño y Edificación</a>
+                                        </td>
+                                        <td><a href="">> Humanidades</a></td>
+                                    </tr>
+                                    <tr>
+
+                                        <td> <a href="">> Matematicás e ingeniería </a>
+                                        </td>
+                                        <td>
+                                            <a href="">> Universidad Abierta y Educación a Distancia </a>
+                                        </td>
+
+                                    </tr>
+                                    <tr>
+
+
+                                        <td> <a href="">> Enseñanza de Idiomas</a>
+                                        </td>
+                                        <td><a href="">> Divulgación Cientifica</a>
+                                        </td>
+                                    </tr>
+
+
+                                </tbody>
+
+                            </table>
+                        </div>
+
+                    </li>
+
+                    <li><a href="#">E-BOOKS</a></li>
+                    <li><a href="#">EVENTOS</a></li>
+
+                    <li><a href="#">BLOG</a></li>
+                    <li><a href="#">CONTACTO</a></li>
+                </ul>
+
+
+
+                <div id="custom-search-input">
+                    <div class="input-group col-md-12">
+                        <input type="text" class="  search-query form-control" placeholder="Buscar en toda la tienda" />
+                        <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                </div>
+
+
+                <!--MENÚ DE TOGGLE,en dispositivos moviles -->
+
+                <div class="container-fluid">
+                    <div class="menu_toggle">
+                        <i class="fa fa-bars fa-2x toggle-btn" data-toggle="collapse" data-target="#menu-toggle-content"></i>
+
+                        <div class="menu-toggle-list">
+
+                            <ul id="menu-toggle-content" class="menu-toggle-content out collapse ">
+
+                                <li>NUESTROS LIBROS</li>
+
+                                <li data-toggle="collapse" data-target="#area-tematica" class="collapsed">
+                                    ÁREA TEMÁTICA <span class="glyphicon glyphicon-plus"></span>
+                                </li>
+
+                                <ul class="sub-menu collapse" id="area-tematica">
+                                    <li>Estudios de Posgrado</li>
+                                    <li>diseño y edificacion</li>
+                                    <li>Matemáticas e ingeniería</li>
+                                    <li>enseñansa de idiomas</li>
+                                    <li>siencias socioeconomicas</li>
+                                    <li>humanidades</li>
+                                    <li>universidad abierta y educacion a distancia</li>
+                                    <li>investigacion aplicada y multidisiplinaria</li>
+                                    <li>divulgación cientifica</li>
+                                </ul>
+
+
+                                <li><a href="#">E-BOOKS</a></li>
+                                <li><a href="#">EVENTOS</a></li>
+                                <li><a href="#">BLOG</a></li>
+                                <li>CONTACTO</li>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </nav>
+
+</div>
+<!--//Walter segundo menu-->
+</header>
---
+++ partes/menu_2.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <section class="row">
+        <div class="col-lg-3 col-md-3 col-sm-3">
+            <ul>
+                <li class="categoria">Categorias</li>
+                <li class="left-menu">Nuestros Libros</li>
+                <li class="left-menu">E-Books</li>
+                <li class="left-menu">Eventos</li>
+
+                <li class="left-menu">Blog</li>
+                <li class="left-menu">Contacto</li>
+            </ul>
+
+            <img src="slider/assets/publicidad.jpg" class="img img-responsive">
+
+        </div>
