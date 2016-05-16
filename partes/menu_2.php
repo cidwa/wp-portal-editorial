2f239cd145143e77aa86d1457c4b5817ffd6fef1
WalterBrake <walter_palacios_mac@hotmail.com>
2016-05-16 00:29:21 -0500

    UPDATES Walter
    menú y functions

--- partes/menu_2.php
+++ partes/menu_2.php
@@ -1,16 +1,56 @@
-<div class="container">
-    <section class="row">
-        <div class="col-lg-3 col-md-3 col-sm-3">
-            <ul>
-                <li class="categoria">Categorias</li>
-                <li class="left-menu">Nuestros Libros</li>
-                <li class="left-menu">E-Books</li>
-                <li class="left-menu">Eventos</li>
+ <!--Walter segundo menu-->
+            <nav class="navbar navbar-default navbar-principal ">
+                <div class="container-fluid">
+                    <ul class="container">
 
-                <li class="left-menu">Blog</li>
-                <li class="left-menu">Contacto</li>
-            </ul>
 
-            <img class="third-slide" src="<?php echo get_template_directory_uri();?>/slider/assets/publicidad.jpg" alt="">
 
-        </div>
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="botton-hamburguer">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                        </div>
+                        <div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="  search-query form-control" placeholder="Buscar en toda la tienda" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                                </button>
+                                </span>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="container nav-menu-principal">
+                            
+                            <?php /* Primary navigation */
+                    wp_nav_menu( array(
+                        'theme_location'=>'menu_principal',
+                    'menu' => 'menu_principal',
+                    'depth' => 3,
+                    'container' => 'container nav-menu-principal',
+                    'menu_class' => 'nav navbar-nav collapse navbar-collapse',
+                     'menu_id' => 'bs-example-navbar',
+                    'walker' => new wp_bootstrap_navwalker()
+                    ));
+
+                    ?>
+                        </div>
+
+
+                    </ul>
+
+                    <!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container-fluid -->
+            </nav>
+
+        <!--//Walter segundo menu-->
