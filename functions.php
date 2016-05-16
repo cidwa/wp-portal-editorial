3fe961a0f5ffd3656f047fb7e9b13758aecb0952
salomonmac <geforcesalomonti@gmail.com>
2016-05-16 13:37:47 -0500

    update salomon

--- functions.php
+++ functions.php
@@ -3,22 +3,22 @@
 function theme_styles(){
     wp_enqueue_style("bs_css", get_stylesheet_directory_uri(). "/css/bootstrap.min.css");
     wp_enqueue_style("style_css", get_stylesheet_directory_uri(). "/style.css");
-    wp_enqueue_style( 'style_footer',get_stylesheet_directory_uri(). '/css/style_footer.css' );
-    wp_enqueue_style( 'style_body2', get_stylesheet_directory_uri(). '/css/style_body2.css' );
-    wp_enqueue_style( 'style_body4', get_stylesheet_directory_uri(). '/css/style_body4.css' );
-    wp_enqueue_style( 'styles-navbar-principal', get_stylesheet_directory_uri(). '/css/styles-navbar-principal.css' );
-    wp_enqueue_style( 'styles_slider', get_stylesheet_directory_uri(). '/css/styles_slider.css' );
-    wp_enqueue_style( 'styles-navbar-toggle', get_stylesheet_directory_uri(). '/css/styles-navbar-toggle.css' );
+    wp_enqueue_style( "style_footer",get_stylesheet_directory_uri(). "/css/style_footer.css" );
+    wp_enqueue_style( "style_body2", get_stylesheet_directory_uri(). "/css/style_body2.css" );
+    wp_enqueue_style( "style_body4", get_stylesheet_directory_uri(). "/css/style_body4.css");
+    wp_enqueue_style( "styles-navbar-principal", get_stylesheet_directory_uri(). "/css/styles-navbar-principal.css" );
+    wp_enqueue_style( "styles_slider", get_stylesheet_directory_uri(). "/css/styles_slider.css" );
+    wp_enqueue_style( "styles-navbar-toggle", get_stylesheet_directory_uri(). "/css/styles-navbar-toggle.css" );
     wp_enqueue_style( "styles-header", get_stylesheet_directory_uri(). "/css/styles-header.css" );
 
 }
 
 function wpt_register_js() {
-    wp_enqueue_script("bootstrap", get_stylesheet_directory_uri().'bootstrap.min.js');    
-    wp_enqueue_script("jquery", get_stylesheet_directory_uri().'jquery.min.js'); 
-    wp_enqueue_script("bootstrap-hover-dropdown", get_stylesheet_directory_uri().'bootstrap-hover-dropdown.min.js');    
-    wp_enqueue_script("jcarousel", get_stylesheet_directory_uri().'jquery.jcarousel.min.js');    
-    wp_enqueue_script("bryan-body2", get_stylesheet_directory_uri().'bryan-body2.js');    
+    wp_enqueue_script("bootstrap", get_stylesheet_directory_uri()."bootstrap.min.js");    
+    wp_enqueue_script("jquery", get_stylesheet_directory_uri()."jquery.min.js"); 
+    wp_enqueue_script("bootstrap-hover-dropdown", get_stylesheet_directory_uri()."bootstrap-hover-dropdown.min.js");    
+    wp_enqueue_script("jcarousel", get_stylesheet_directory_uri()."jquery.jcarousel.min.js");    
+    wp_enqueue_script("bryan-body2", get_stylesheet_directory_uri()."bryan-body2.js");    
 }
 
 add_action("wp_enqueue_scripts","theme_styles");
