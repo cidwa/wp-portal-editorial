6700c7ad457079b510f2464036548bd526a6b38e
WalterBrake <walter_palacios_mac@hotmail.com>
2016-05-15 19:03:09 -0500

    updates

---
+++ partes/menu.php
@@ -0,0 +1,74 @@
+
+        <!--Walter segundo menu-->
+            <nav class="navbar navbar-default navbar-principal ">
+                <div class="container-fluid">
+                    <ul class="container">
+
+
+
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="botton-hamburguer">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+
+                        </div>
+                        <div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="  search-query form-control" placeholder="Buscar en toda la tienda" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-danger" type="button">
+                                        <span class=" glyphicon glyphicon-search"></span>
+                                </button>
+                                </span>
+                            </div>
+                        </div>
+
+
+
+
+                        <div class="container nav-menu-principal">
+                            <ul class="nav navbar-nav collapse navbar-collapse" id="bs-example-navbar">
+                                <li><a href="#">NUESTROS LIBROS</a></li>
+
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">AREA TEMATICA<span class="glyphicon glyphicon-plus"></span></a>
+
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Estudios de Posgrado</a></li>
+                                        <li><a href="#">diseño y edificacion</a></li>
+                                        <li><a href="#">enseñansa de idiomas</a></li>
+                                        <li><a href="#">ciencias socioeconomicas</a></li>
+                                        <li><a href="#">humanidades</a></li>
+                                        <li><a href="#">investigacion aplicada y multidisiplinaria</a></li>
+                                        <li><a href="#">divulgación cientifica</a></li>
+
+
+                                    </ul>
+                                </li>
+
+
+
+                                <li><a href="#">E-BOOKS</a></li>
+                                <li><a href="#">EVENTOS</a></li>
+                                <li><a href="#">BLOG</a></li>
+                                <li><a href="#">CONTACTO</a></li>
+
+
+                            </ul>
+
+
+                        </div>
+
+
+                    </ul>
+
+                    <!-- /.navbar-collapse -->
+                </div>
+                <!-- /.container-fluid -->
+            </nav>
+
+        <!--//Walter segundo menu-->
