87a2d69e6ccdc7a339569ee80b488254e2e56e68
tavoTd <tavo_decadente14@hotmail.com>
2016-05-17 00:41:38 -0500

    add gus

---
+++ home.php
@@ -0,0 +1,150 @@
+<?php
+ get_header();
+?>
+    <div class="container">
+        <section>
+            <div class="col-lg-3 col-md-3 col-sm-3">
+                <ul>
+                    <li class="categoria">Categorias</li>
+                    <li class="left-menu">Nuestros Libros</li>
+                    <li class="left-menu">E-Books</li>
+                    <li class="left-menu">Eventos</li>
+
+                    <li class="left-menu">Blog</li>
+                    <li class="left-menu">Contacto</li>
+                </ul>
+                <img src="<?php bloginfo('template_url');?>/slider/assets/publicidad.jpg" class="img img-responsive">
+            </div>
+            <div class="col-lg-9 col-md-9 col-sm-9">
+                <div class="page-tittle">
+                    <h1>Novedades</h1>
+                </div>
+                <div class="col-md-12">
+                    <img src="http://www.libros.unam.mx/media//NOVEDADES.jpg" alt="" class="img img-responsive">
+                </div>
+                <div class="col-md-12">
+
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070272141.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">Hacia ciudades más seguras</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 300</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070256363.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">Tránsitos y apropiaciones</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 211</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070265341.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">Tratado de hermenéutica analógica. Hacia un nuevo modelo de interpretación</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 200</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+
+                    <div class="clearfix visible-md-block"></div>
+
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070261381.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">La querella de Martín Luis Guzmán</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 300</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786078348886.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">La integración excluyente</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 160</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070272233.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">A tiro de fuego</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 336</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="clearfix visible-md-block"></div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070271380.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">José Vasconcelos: alfabetización, blibliotecas, lectura y edición</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 348</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070272868.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12">
+                            <p class="text-center"style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">La música delgada</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 160</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+                    <div class="clearfix visible-sm-block"></div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <img src="http://www.libros.unam.mx/Portadas/9786070272776.jpg" alt="" class="img img-responsive" style="width:192.5px; height: 265px;">
+                        <div class="col-md-12" style="overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                            <p class="text-center">Bibliomanía</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center" style="font-weight: bold;">MXN$ 67</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="text-center"><span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span></p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </div>
+<?php
+ get_footer();
+?>
