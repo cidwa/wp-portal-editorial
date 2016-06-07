d3d556c6a723321cb7dbe38c2992ebf5d7512ac3
jcrunge <juan.crunge@gmail.com>
2016-06-07 14:16:46 -0500

    Foot

---
+++ footer.php
@@ -0,0 +1,61 @@
+<footer class="container-fluid">
+        <div class="row aire">
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <!--Columna 2-->
+                <h4>Lineamiento</h4>
+                <ul>
+                    <li class="espli">&gt; <a href="comite-editorial-interno.html">Comite Editorial interno</a></li>
+                    <li class="espli">&gt; <a href="comite-editorial.html">Comite editorial</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <!--Columna 3-->
+                <h4>Integración</h4>
+                <ul>
+                    <li class="espli">&gt; <a href="integracion.html">Integración</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <!--Columna 4-->
+                <h4>Sistema</h4>
+                <ul>
+                    <li class="espli">&gt; <a href="http://sistemas.acatlan.unam.mx/publicaciones/">Sistema</a></li>
+
+                </ul>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                <aside>
+                    <h4>Siguenos en:</h4>
+                    <a href="https://www.facebook.com/" target="_blank"><img alt="Siguenos en Facebook" src="<?php echo get_template_directory_uri();?>img/facebook.png" width=32 height=32 /></a>
+
+                    <a href="https://twitter.com/EdicionAcatlan" target="_blank"><img src="<?php echo get_template_directory_uri();?>img/twitter.png" width=32 height=32 alt="Síguenos en Twitter" /></a>
+
+                </aside>
+            </div>
+
+        </div>
+    </footer>
+
+    <div class="container-fluid">
+        <div class="row footer2">
+
+
+            <p class="text-center">Hecho en México, todos los derechos reservados 2016. Esta página puede ser reproducida con fines no lucrativos, siempre y cuando no se mutile, se cite la fuente completa y su dirección electrónica. De otra forma, requiere permiso previo por escrito de la institución. <a href="creditos.html">Créditos</a></p>
+            <div class="footer3">
+                <p class="text-center">Sitio Web Administrado por el Departamento de Servicio de Computo</p>
+            </div>
+
+
+        </div>
+    </div>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-hover-dropdown/2.2.1/bootstrap-hover-dropdown.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="js/jquery.jcarousel.min.js"></script>
+    <script src="js/bryan-body2.js"></script>
+    <?php wp_footer(); ?>
+</body>
+
+</html>
