21cc419cde70899783c857c151f9b525c6ff2c9f
Diego Hernández <goo.artpop@gmail.com>
2016-06-07 14:13:36 -0500

    Corrección -Diego

--- functions.php
+++ functions.php
@@ -4,14 +4,13 @@ function cargar_css_del_tema(){
 
     wp_enqueue_style("bootstrap", get_stylesheet_directory_uri(). "/css/bootstrap.min.css");
     wp_enqueue_style("style", get_stylesheet_directory_uri(). "/style.css");
-    wp_enqueue_style( 'style_footer',get_stylesheet_directory_uri(). '/css/style_footer.css' );
-    wp_enqueue_style( 'style_body2', get_stylesheet_directory_uri(). '/css/style_body2.css' );
-    wp_enqueue_style( 'style_body4', get_stylesheet_directory_uri(). '/css/style_body4.css' );
-    wp_enqueue_style( 'styles-navbar-principal', get_stylesheet_directory_uri(). '/css/styles-navbar-principal.css' );
-    wp_enqueue_style( 'styles_slider', get_stylesheet_directory_uri(). '/css/styles_slider.css' );
-    wp_enqueue_style( 'styles-navbar-toggle', get_stylesheet_directory_uri(). '/css/styles-navbar-toggle.css' );
-    wp_enqueue_style( "styles-header", get_stylesheet_directory_uri(). "/css/styles-header.css" );
-    wp_enqueue_style( "light", get_stylesheet_directory_uri(). "/css/light.css" );
+    wp_enqueue_style("style_footer",get_stylesheet_directory_uri(). '/css/style_footer.css' );
+    wp_enqueue_style("style_body2", get_stylesheet_directory_uri(). '/css/style_body2.css' );
+    wp_enqueue_style("style_body4", get_stylesheet_directory_uri(). '/css/style_body4.css' );
+    wp_enqueue_style("styles-navbar-principal", get_stylesheet_directory_uri(). '/css/styles-navbar-principal.css' );
+    wp_enqueue_style("styles_slider", get_stylesheet_directory_uri(). '/css/styles_slider.css' );
+    wp_enqueue_style("styles-header", get_stylesheet_directory_uri(). "/css/styles-header.css" );
+    wp_enqueue_style("light", get_stylesheet_directory_uri(). "/css/light.css" );
 }
 
  function cargar_js_del_tema(){
@@ -22,12 +21,12 @@ function cargar_css_del_tema(){
     wp_enqueue_script("bryan-body2", get_stylesheet_directory_uri()."/js/bryan-body2.js");
     wp_enqueue_script("mityped", get_stylesheet_directory_uri()."/js/mityped.js");
     wp_enqueue_script("npm", get_stylesheet_directory_uri()."/js/npm.js");
+    wp_enqueue_script("pdfobject", get_stylesheet_directory_uri()."/js/pdfobject.js");
+    wp_enqueue_script("pdfobject1", get_stylesheet_directory_uri()."/js/pdfobject.min.js");
     wp_enqueue_script("typed", get_stylesheet_directory_uri()."/js/typed.js");
 }
 add_action("wp_enqueue_scripts","cargar_js_del_tema");
- add_action("wp_enqueue_scripts","cargar_css_del_tema");
-
-
+add_action("wp_enqueue_scripts","cargar_css_del_tema");
 
 /* ***FIN***Funciones para cargar estilo y scripts */
 
