9e8eaf00add81544953921f2e3113c64ab841acf
Diego Hernández <goo.artpop@gmail.com>
2016-07-16 00:15:03 -0500

    slider

--- front-page.php
+++ front-page.php
@@ -2,6 +2,79 @@
 get_header();
 ?>
     <!--//Slider de Diego-->
+    <div class="container">
+        <section class="row">
+            <div class="col-sm-2"></div>
+            <div class=" col-sm-8">
+                <div>
+                    <!--SLider-->
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                            <li data-target="#myCarousel" data-slide-to="4"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img src="slider/assets/1.jpg" alt="">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="slider/assets/2.jpg" alt="">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="slider/assets/3.jpg" alt="">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="slider/assets/4.jpg" alt="">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img src="slider/assets/5.jpg" alt="">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <!-- /.carousel -->
+                </div>
+            </div>
+            <div class="col-sm-2"></div>
+        </section>
+    </div>
+    <!--//Slider de Diego-->
     <div class="container espacio">
         <div class="row">
             <div class="col-lg-4 col-md-4 col-xs-12 ">
--- index.php
+++ index.php
@@ -6,13 +6,86 @@ get_header();
           <h2>estamos en index.php</h2>
                 <!--estamos en index.php-->
 
+<!--//Slider de Diego-->
+<div class="container">
+        <section class="row">
+            <div class="col-sm-2"></div>
+            <div class=" col-sm-8">
+                <div>
+                    <!--SLider-->
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#myCarousel" data-slide-to="1"></li>
+                            <li data-target="#myCarousel" data-slide-to="2"></li>
+                            <li data-target="#myCarousel" data-slide-to="3"></li>
+                            <li data-target="#myCarousel" data-slide-to="4"></li>
+                        </ol>
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item active">
+                                <img class="first-slide" src="slider/assets/1.jpg" alt="Tema">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="second-slide" src="slider/assets/2.jpg" alt="Pollo">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="third-slide" src="slider/assets/3.jpg" alt="Pescado">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="third-slide" src="slider/assets/4.jpg" alt="Pescado">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <img class="third-slide" src="slider/assets/5.jpg" alt="Pescado">
+                                <div class="container">
+                                    <div class="carousel-caption">
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    </div>
+                    <!-- /.carousel -->
+                </div>
+            </div>
+            <div class="col-sm-2"></div>
+        </section>
+    </div>
     <!--//Slider de Diego-->
     
 <!-- Primera parte -->
  <div class="container">
         <div class="col-md-12">
             <div class=" titulo row">
-                <h3 class="col-md-10 col-sm-10 col-xs-8">Libros del Mes</h3>
+                <h3 class="col-md-10 col-sm-10 col-xs-8">Libros del PUTO Mes</h3>
                 <div class="col-md-2 col-sm-2 col-xs-4">
                     <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
                     <a href="#" class="jcarousel-control-next">&rsaquo;</a>
