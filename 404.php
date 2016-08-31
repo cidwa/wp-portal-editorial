fe240805736fb4c8e07226e0ae5b4cc4dda8af49
Diego Hernández <goo.artpop@gmail.com>
2016-08-31 00:20:26 -0500

    Error 404 page added

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php get_header(); ?>
+
+	<div class="col-sm-12 col-md-12 col-lg-12">
+
+		<div class="col-sm-12 col-md-12 col-lg-12">
+
+			<main class="col-sm-12 col-md-12 col-lg-12">
+
+				<article>
+
+					<header>
+
+						<h1><?php _e( 'Error 404 - Página no encontrada :('); ?></h1>
+
+					</header>
+
+					<section>
+
+						<p><?php _e( 'La página que estás buscando, no fue encontrada, ¡vuelve a intentarlo otra vez!'); ?></p>
+
+					</section>
+
+				</article>
+
+			</main>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
